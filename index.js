fea8341a9f566762cfe905e7d2d176b903499824
Demetrius-Price02 <maskedprofessor64@gmail.com>
2024-10-06 11:36:23 -0500

    made-node-dice-roller

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+app = express()
+
+const cors = require("cors")
+
+var url = require('url');
+var dt = require('./date-time');
+
+const port = process.env.PORT || 3000
+const majorVersion = 1
+const minorVersion = 3
+
+// Use Express to publish static HTML, CSS, and JavaScript files that run in the browser. 
+app.use(express.static(__dirname + '/static'))
+app.use(cors({ origin: '*' }))
+
+// Return rnadom number between 1 and 6.
+app.get('/d6', (request, response) => {
+	console.log('Calling "/d6" on the Node.js server.')
+	response.type('text/plain')
+	response.send(Math.floor(Math.random() * 6 + 1))
+})
