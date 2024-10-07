c4e65d61a1a9d8c7a9f89cc47ff3b59d66dbfb65
Demetrius-Price02 <maskedprofessor64@gmail.com>
2024-10-07 13:47:35 -0500

    p

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ app.get('/d6', (request, response) => {
 	response.json({Number: ran})
 })
 
+// Custom 404 page.
+app.use((request, response) => {
+	response.type('text/plain')
+	response.status(404)
+	response.send('404 - Not Found')
+  })
+  
+  // Custom 500 page.
+  app.use((err, request, response, next) => {
+	console.error(err.message)
+	response.type('text/plain')
+	response.status(500)
+	response.send('500 - Server Error')
+  })
+
 app.listen(port, () => console.log(
 	`Express started at \"http://localhost:${port}\"\n` +
 	`press Ctrl-C to terminate.`)
